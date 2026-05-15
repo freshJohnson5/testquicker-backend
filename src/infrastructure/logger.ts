bd190e867044c7cbd80ccee22022821e417282a1
ken cote <kencote@kens-MacBook-Air.local>
2026-05-15 11:40:50 -0600

    Fix logger for production

--- src/infrastructure/logger.ts
+++ src/infrastructure/logger.ts
@@ -1,9 +1,13 @@
 import { configure, format, transports } from 'winston'
 
+const logTransports = process.env.NODE_ENV === 'production'
+  ? [new transports.Console()]
+  : [new transports.File({ filename: process.env.API_LOG_FILENAME })]
+
 configure({
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.json()
   ),
-  transports: [new transports.File({ filename: process.env.API_LOG_FILENAME })]
+  transports: logTransports
 })
