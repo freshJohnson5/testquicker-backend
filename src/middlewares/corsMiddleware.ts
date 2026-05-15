0c43b3f123fd9c12d76af1f8bf75f7a65248190e
freshJohnson5 <kennycote924@gmail.com>
2026-05-15 13:02:36 -0600

    Fix CORS for production

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,7 +1,20 @@
 import cors from 'cors'
 import { StatusCodes } from 'http-status-codes'
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://testquicker-frontend.vercel.app'
+]
+
 export const corsMiddleware = cors({
-  origin: process.env.CLIENT_URL,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   optionsSuccessStatus: StatusCodes.OK
 })
